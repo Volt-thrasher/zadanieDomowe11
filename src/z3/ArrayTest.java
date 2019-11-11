bcd1e16ca5d5b1131ca390c5ddb7aef8d58050a4
Volt <sonianadashkevich@gmail.com>
2019-11-11 13:16:02 +0100

    zadanie1 poprawione zadanie2 poprawione

--- src/z3/ArrayTest.java
+++ src/z3/ArrayTest.java
@@ -5,41 +5,52 @@ import java.util.Arrays;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7};
-        System.out.println(Arrays.toString(divideArray(array,4)));
-    }
-
-    private static int[] divideArray (int[] array, int n){
-        int[] sums = new int[array.length/n];
-        if (n>1 && n<=array.length/2){
+        int[] array = {1,2,3,4,5,6,7,8};
+        int n = 3;
+        int limit;
+        boolean even = array.length%2==0;
+        if (even){
+            limit = array.length/2;
+        }else {
+            limit = 1+array.length/2;
+        }
+        if (n>1 && n<=limit){
+            int counter = 1+array.length/n;
+            int[] sums = new int[n];
             File file = new File("array.txt");
             try (BufferedWriter bfw = new BufferedWriter(new FileWriter(file))){
-                BufferedReader bfr = new BufferedReader(new FileReader(file));
-                int counter = array.length/n;
-                for (int i = 0; i<array.length;i++) {
-                    while (i<counter-i) {
-                        bfw.write(array[i]);
+                for (int i = 0; i<array.length;) {
+                    while (i<counter&&i<array.length) {
+                        bfw.write(array[i]+";");
                         i++;
-                        counter*=2;
                     }
+                    counter+=1+array.length/n;
                     bfw.newLine();
                 }
+                bfw.close();
+                BufferedReader bfr = new BufferedReader(new FileReader(file));
                 String line;
+                counter = n;
                 while ((line=bfr.readLine())!=null){
                     String[] numbers = line.split(";");
-                    for (String number:numbers) {
-                        int sum = 0;
-                        sum += Integer.valueOf(number);
-                        while (counter>0){
-                            sums[sums.length-counter]=sum;
-                            counter--;
+                    int sum = 0;
+                    while (counter>0){
+                        for (String number:numbers) {
+                        sum += Integer.parseInt(number);
                         }
+                    sums[sums.length-counter]=sum;
+                        counter--;
+                        break;
                     }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            System.out.println(Arrays.toString(sums));
         }
-        return sums;
     }
-}
+    }
+
+//    private static int[] divideArray (int[] array, int n){
+//
+//}
