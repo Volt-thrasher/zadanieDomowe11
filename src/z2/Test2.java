9198cf74f9c3aac481abf712a1644581b10e1264
Volt <sonianadashkevich@gmail.com>
2019-11-11 17:00:16 +0100

    zadanie1 poprawione zadanie2 wydzielono metode zadanie3

--- src/z2/Test2.java
+++ src/z2/Test2.java
@@ -1,41 +1,43 @@
 package z2;
 
-import z1.EmployeeTest;
-
 import java.io.*;
 
 public class Test2 {
     public static void main(String[] args) {
         File file = new File("arithmetic.txt");
-        File file1 = new File("results.txt");
+        calculateFromFile(file);
+    }
+
+    private static void calculateFromFile(File file) {
         String line;
         try (BufferedReader bfr = new BufferedReader(new FileReader(file))) {
-            BufferedWriter bfw = new BufferedWriter(new FileWriter(file1));
-                while ((line = bfr.readLine()) != null) {
-                    String[] signs = line.split(" ");
-                    double result = 0;
-                    String sign = null;
-                    switch (signs[1]) {
-                        case "+":
-                            result = Double.valueOf(signs[0]) + Double.valueOf(signs[2]);
-                            sign = "+";
-                            break;
-                        case "-":
-                            result = Double.valueOf(signs[0]) - Double.valueOf(signs[2]);
-                            sign = "-";
-                            break;
-                        case "*":
-                            result = Double.valueOf(signs[0]) * Double.valueOf(signs[2]);
-                            sign = "*";
-                            break;
-                        case "/":
-                            result = Double.valueOf(signs[0]) / Double.valueOf(signs[2]);
-                            sign = "/";
-                            break;
-                    }
-                    bfw.write(signs[0] + sign + signs[2] + "=" + result);
-                    bfw.newLine();
+            while ((line = bfr.readLine()) != null) {
+                String[] signs = line.split(" ");
+                double result = 0;
+                String sign = null;
+                switch (signs[1]) {
+                    case "+":
+                        result = Double.valueOf(signs[0]) + Double.valueOf(signs[2]);
+                        sign = "+";
+                        break;
+                    case "-":
+                        result = Double.valueOf(signs[0]) - Double.valueOf(signs[2]);
+                        sign = "-";
+                        break;
+                    case "*":
+                        result = Double.valueOf(signs[0]) * Double.valueOf(signs[2]);
+                        sign = "*";
+                        break;
+                    case "/":
+                        result = Double.valueOf(signs[0]) / Double.valueOf(signs[2]);
+                        sign = "/";
+                        break;
                 }
+                File file1 = new File("results.txt");
+                BufferedWriter bfw = new BufferedWriter(new FileWriter(file1));
+                bfw.write(signs[0] + sign + signs[2] + "=" + result);
+                bfw.newLine();
+            }
 
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
