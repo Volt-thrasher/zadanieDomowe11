ffcd4147e10105f93a87c56e271c552c28aec54b
Volt <sonianadashkevich@gmail.com>
2019-11-11 18:07:48 +0100

    zadanie1 zadanie2 wydzielono metode zadanie3 poprawione

--- src/z3/ArrayTest.java
+++ src/z3/ArrayTest.java
@@ -1,18 +1,87 @@
 package z3;
 
+import javax.naming.LimitExceededException;
 import java.io.*;
 import java.util.Arrays;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] array = {1, 2, 3, 4, 5, 6};
         int n = 3;
-        int[] newArray = divideArray(array, n);
+        int[] newArray = new int[0];
+        try {
+            newArray = divideArray(array, n);
+        } catch (LimitExceededException e) {
+            System.err.println(e.getMessage());
+        }
         System.out.println(Arrays.toString(newArray));
     }
 
-    private static int[] divideArray(int[] array, int n) {
+    private static int[] divideArray(int[] array, int n) throws LimitExceededException {
         int[] sums = new int[n];
+        int limit = getLimit(array);
+        if (n > 1 && n <= limit) {
+            int counter = getCounter(array, n);
+            File file = new File("array.csv");
+            try (BufferedWriter bfw = new BufferedWriter(new FileWriter(file))) {
+                arrayToCsv(array, n, counter, bfw);
+                BufferedReader bfr = new BufferedReader(new FileReader(file));
+                String line;
+                counter = n;
+                fillSumsArray(sums, counter, bfr);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            throw new LimitExceededException("Podzial tabeli na n czesci jest niemozliwy. \n " +
+                    "Wartosc n powinna byc mniejsza od polowy dlugosci tabeli.");
+        }
+        return sums;
+    }
+
+    private static void fillSumsArray(int[] sums, int counter, BufferedReader bfr) throws IOException {
+        String line;
+        while ((line = bfr.readLine()) != null) {
+            String[] numbers = line.split(";");
+            int sum = 0;
+            while (counter > 0) {
+                for (String number : numbers) {
+                    sum += Integer.parseInt(number);
+                }
+                sums[sums.length - counter] = sum;
+                counter--;
+                break;
+            }
+        }
+    }
+
+    private static void arrayToCsv(int[] array, int n, int counter, BufferedWriter bfw) throws IOException {
+        for (int i = 0; i < array.length; ) {
+            while (i < counter && i < array.length) {
+                bfw.write(array[i] + ";");
+                i++;
+            }
+            if (array.length%n==0){
+                counter+=array.length/n;
+            }else {
+                counter+=1+array.length/n;
+            }
+            bfw.newLine();
+        }
+        bfw.close();
+    }
+
+    private static int getCounter(int[] array, int n) {
+        int counter;
+        if (array.length%n!=0) {
+            counter = 1 + array.length / n;
+        }else {
+            counter = array.length/n;
+        }
+        return counter;
+    }
+
+    private static int getLimit(int[] array) {
         int limit;
         boolean even = array.length % 2 == 0;
         if (even) {
@@ -20,38 +89,6 @@ public class ArrayTest {
         } else {
             limit = 1 + array.length / 2;
         }
-        if (n > 1 && n <= limit) {
-            int counter = 1 + array.length / n;
-            File file = new File("array.txt");
-            try (BufferedWriter bfw = new BufferedWriter(new FileWriter(file))) {
-                for (int i = 0; i < array.length; ) {
-                    while (i < counter && i < array.length) {
-                        bfw.write(array[i] + ";");
-                        i++;
-                    }
-                    counter += 1 + array.length / n;
-                    bfw.newLine();
-                }
-                bfw.close();
-                BufferedReader bfr = new BufferedReader(new FileReader(file));
-                String line;
-                counter = n;
-                while ((line = bfr.readLine()) != null) {
-                    String[] numbers = line.split(";");
-                    int sum = 0;
-                    while (counter > 0) {
-                        for (String number : numbers) {
-                            sum += Integer.parseInt(number);
-                        }
-                        sums[sums.length - counter] = sum;
-                        counter--;
-                        break;
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sums;
+        return limit;
     }
 }
